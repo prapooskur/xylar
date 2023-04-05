1e7e739559a12b5891e0c30502844d3ad0b9a34e
prapooskur <prapooskur@gmail.com>
2023-04-05 02:04:27 +0000

    fixed file handling

--- xylar.py
+++ xylar.py
@@ -18,8 +18,6 @@ bot = commands.Bot(command_prefix="/",intents=intents)
 
 sdurl = os.getenv('SDURL')
 
-sizes = (1,2,3,4)
-
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
@@ -39,10 +37,26 @@ async def simulate(interaction: discord.Interaction, prompt: str, negativeprompt
         "batch_size": 1,
         "seed": seed
     }
+    if (negativeprompt == ""):
+        print("requesting prompt "+prompt+" with no negative prompt")
+    else:
+        print("requesting prompt "+prompt+" with negative prompt "+negativeprompt)
     response = requests.post(url=f'{sdurl}/sdapi/v1/txt2img', json=payload)
-    images=response.json().get("images","")
-    for i in images:
-        image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
-    await interaction.response.send_message(file=image)
+    imagelist=response.json().get("images","")
+    for i in imagelist:
+        outputimg = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+        outputimg.save('output.png')
+    await interaction.response.send_message(file=discord.File(outputimg))
+
+@bot.tree.command(name='sync', description='sync commands with discord')
+async def sync(interaction: discord.Interaction):
+    try:
+        synced=await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+        interaction.response.send_message(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(f"error syncing commands: {e}")
+        interaction.response.send_message(f"error syncing commands: {e}")
+
 
 bot.run(os.getenv('TOKEN'))
